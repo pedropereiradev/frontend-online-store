bd177fb5a805f2ba65eb4a0301b1ff27540b8cb1
Matheus Monteiro <matheuschran@hotmail.com>
2022-04-26 16:19:11 -0300

    arruma erros do lint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import * as api from './services/api';
+// import * as api from './services/api';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {
-        // api.getCategories().then(categories => {console.log(categories)})
-        api.getProductsFromCategoryAndQuery('MLB1055', 'motorola').then(categories => {console.log(categories)})
-      }
+      <p>app</p>
     </div>
   );
 }
