fbdec48567676676639e24cee9ac8c2473df2426
Rick <hoa98hoa@gmail.com>
2022-04-26 19:57:18 -0300

    Adc botão

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Cart from './pages/Cart';
+import Search from './pages/Search';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ Search } exact />
+          <Route path="/cart" component={ Cart } exact />
+        </Switch>
+      </BrowserRouter>
     );
   }
+}
 
 export default App;
