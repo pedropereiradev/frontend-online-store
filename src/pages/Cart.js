479055354eec7019939f7de146a0765780547eeb
Pedro Pereira <pedro.2108@hotmail.com>
2022-04-27 16:19:23 -0300

    Implimentado função de remover item do carrinho

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { getCartProducts } from '../services/cartApi';
+import { getCartProducts, removeCartItem } from '../services/cartApi';
 
 class Cart extends React.Component {
-  render() {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
     const products = getCartProducts();
 
+    this.setState({ products });
+  }
+
+  handleClick(product) {
+    removeCartItem(product);
+    const products = getCartProducts();
+
+    this.setState({ products });
+  }
+
+  render() {
+    const { products } = this.state;
     return !products ? (
       <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
     ) : (
@@ -17,7 +39,12 @@ class Cart extends React.Component {
               alt={ `Imagem do produto ${product.title}` }
             />
             <span>{product.price}</span>
-            <span data-testid="shopping-cart-product-quantity">Quantidade: 1</span>
+            <span data-testid="shopping-cart-product-quantity">
+              Quantidade: 1
+            </span>
+            <button type="button" onClick={ () => this.handleClick(product) }>
+              Remover
+            </button>
           </li>
         ))}
       </ul>
