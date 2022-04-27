77ca99720f22ed74974609108700018a4082f121
Pedro Pereira <pedro.2108@hotmail.com>
2022-04-27 16:07:51 -0300

    Realizado requisito 8

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { getCartProducts } from '../services/cartApi';
 
 class Cart extends React.Component {
   render() {
-    return (
+    const products = getCartProducts();
+
+    return !products ? (
       <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+    ) : (
+      <ul>
+        {products.map((product) => (
+          <li key={ product.id }>
+            <h3 data-testid="shopping-cart-product-name">{product.title}</h3>
+            <img
+              src={ product.thumbnail }
+              alt={ `Imagem do produto ${product.title}` }
+            />
+            <span>{product.price}</span>
+            <span data-testid="shopping-cart-product-quantity">Quantidade: 1</span>
+          </li>
+        ))}
+      </ul>
     );
   }
 }
