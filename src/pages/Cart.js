e5459fc7db0e75b30a7de98bd2079c927f77eef7
Rick <hoa98hoa@gmail.com>
2022-04-28 09:53:53 -0300

    Adc Logica para carrinho vazio

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import CartProduct from '../components/CartProduct';
 
 class Cart extends React.Component {
+  constructor() {
+    super();
+    this.state = { isEmpty: false };
+  }
+
   render() {
-    return (
-      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+    const { isEmpty } = this.state;
+    if (isEmpty) {
+      return (
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      );
+    } return (
+      <CartProduct />
     );
   }
 }
