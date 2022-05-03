d21b78ccdba6c9a2f829b868e71aa9609be52cdd
Rick <hoa98hoa@gmail.com>
2022-05-03 20:15:07 -0300

    Criado um metodo para ordenar produtos

---
+++ src/components/SelectedOrder.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './SelectedOrder.module.css';
+
+class SelectedOrder extends Component {
+  render() {
+    const { handleChangeSelect } = this.props;
+    return (
+      <select className={ styles.select } onChange={ handleChangeSelect }>
+        <option>Order</option>
+        <option>Menos</option>
+        <option>Maior</option>
+      </select>
+    );
+  }
+}
+
+SelectedOrder.propTypes = {
+  handleChangeSelect: PropTypes.func.isRequired,
+};
+
+export default SelectedOrder;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,8 @@ import Products from '../components/Products';
 import { getProductsFromCategoryAndQuery } from '../services/api';
 import { setNewCartProduct } from '../services/cartApi';
 import styles from './Home.module.css';
+import { sortLowerToHigher, sortHigherToLower } from '../services/order';
+import SelectedOrder from '../components/SelectedOrder';
 
 class Home extends Component {
   constructor() {
@@ -51,6 +53,15 @@ class Home extends Component {
       });
     });
   }
+
+  handleChangeSelect = ({ target }) => {
+    const { products } = this.state;
+    if (target.value === 'Menos') {
+      this.setState({ products: sortLowerToHigher(products) });
+    } else if (target.value === 'Maior') {
+      this.setState({ products: sortHigherToLower(products) });
+    }
+  };
 
   addToCart(productId) {
     const { products } = this.state;
@@ -105,11 +116,16 @@ class Home extends Component {
             renderProductsByCategory={ this.renderProductsByCategory }
           />
           <section className={ styles.productsSection }>
-            <Form
-              search={ search }
-              handleChange={ this.handleChange }
-              onSearchButtonClick={ this.onSearchButtonClick }
-            />
+            <div>
+              <Form
+                search={ search }
+                handleChange={ this.handleChange }
+                onSearchButtonClick={ this.onSearchButtonClick }
+              />
+              <SelectedOrder
+                handleChangeSelect={ this.handleChangeSelect }
+              />
+            </div>
             {isLoading ? (
               <Loading />
             ) : (
