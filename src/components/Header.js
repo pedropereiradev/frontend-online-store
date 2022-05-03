e1eeea8999fab878809950f4a97c61ca03be4bbe
Pedro Pereira <pedro.2108@hotmail.com>
2022-05-03 15:38:07 -0300

    Remove go back button and logo linked to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import BntCarrinho from './BntCarrinho';
 import logo from '../assets/logo.svg';
 import styles from './Header.module.css';
 
 class Header extends React.Component {
   render() {
-    const { actualRoute, goBack, updateCart } = this.props;
+    const { updateCart } = this.props;
 
     return (
       <header className={ styles.header }>
-        <section className={ styles.logo }>
-          <img src={ logo } alt="logo" />
-          <h1>Trybe Store</h1>
-        </section>
-        {actualRoute !== '/' && (
-          <button
-            type="button"
-            onClick={ goBack }
-          >
-            Voltar
-          </button>
-        )}
+        <Link to="/">
+          <section className={ styles.logo }>
+            <img src={ logo } alt="logo" />
+            <h1>Trybe Store</h1>
+          </section>
+        </Link>
         <BntCarrinho updateCart={ updateCart } />
       </header>
     );
@@ -29,8 +24,6 @@ class Header extends React.Component {
 }
 
 Header.propTypes = {
-  goBack: PropTypes.func.isRequired,
-  actualRoute: PropTypes.string.isRequired,
   updateCart: PropTypes.bool.isRequired,
 };
 
